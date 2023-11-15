6271ce5c4352eb3ab784c30b1325ff4951a9de54
MatiroWasTaken <matias301020000@gmail.com>
2023-11-15 08:43:49 -0300

    adds changes to config and postman collection

---
+++ config.js
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+export const PORT = 4000;
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const TOKEN_SECRET = "secret";
+
+export const FRONTEND_URL = "http://localhost:3000";
