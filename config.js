adf94a83d9f5f09f89a0ca5a812db28af0af8c6c
Matiro00 <74512382+Matiro00@users.noreply.github.com>
2023-11-15 01:25:34 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = 4000;
-export const MONGODB_URI = "mongodb+srv://Matiro:2A7SZ30h46lFbbQV@cluster0.wvvknju.mongodb.net/?retryWrites=true&w=majority";
-export const TOKEN_SECRET = "secret";
-
-export const FRONTEND_URL = "http://localhost:3000";
